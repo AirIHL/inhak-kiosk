105d7f8d6411a340b5e015b34ab55d136fa16a8d
AirIHL <inhak1122@gmail.com>
2025-01-13 21:06:59 +0900

    kiosk3 project first try

--- src/main/java/com/example/kiosk3/Kiosk.java
+++ src/main/java/com/example/kiosk3/Kiosk.java
@@ -1,25 +1,21 @@
-package com.example.kiosk2;
+package com.example.kiosk3;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Kiosk {
+    private final List<MenuItem> menuItems;
 
-        List<MenuItem> menuItems = new ArrayList<>();   // List 선언
+    public Kiosk(List<MenuItem> menuItems) {
+        this.menuItems = menuItems;
+    }
 
-        // add 함수를 통해 new MenuItem(이름, 가격, 설명) List에 삽입
-        menuItems.add(new MenuItem("ShakeBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("SmokeShake", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("CheeseBurger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("HamBurger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+    public void start() {
 
-        // Scanner 선언
         Scanner scan = new Scanner(System.in);
-
         boolean running = true;     // while문의 조건 boolean
-        while(running) {
+
+        while (running) {
 
             // 출력 부분
             System.out.println("[ SHAKE SHAKE MENU ]");
---
+++ src/main/java/com/example/kiosk3/MenuItem.java
@@ -0,0 +1,17 @@
+package com.example.kiosk3;
+
+public class MenuItem {
+    private final String name;
+    private final double price;
+    private final String describe;
+
+    public MenuItem(String name, double price, String describe) {
+        this.name = name;
+        this.price = price;
+        this.describe = describe;
+    }
+
+    public String getName() {return name;}
+    public double getPrice() {return price;}
+    public String getDescribe() {return describe;}
+}
